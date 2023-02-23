6f4e3c383277f155432fb8980eae46d2cd61b6bb
赵乾程 <zl18180276692@outlook.com>
2023-02-23 13:36:36 +0800

    fix

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const Koa = require('koa')
-const serve = require('koa-static')
+const Koa = require('koa')//koa框架
+const serve = require('koa-static')//静态资源处理
 const { historyApiFallback } = require('koa2-connect-history-api-fallback') //历史模式配置模块
-// const proxy = require('koa2-proxy-middleware') //代理模块
+const proxy = require('koa2-proxy-middleware') //代理模块
+
+const app = new Koa()//new 一个服务器对象
 /* gzip压缩配置 start */
 const compress = require('koa-compress')
 const options = {
   threshold: 10 //数据超过1mb时压缩
 }
-
-const app = new Koa()
-
 app.use(compress(options))
 /* gzip压缩配置 end */
 
-// 历史模式配置
+// 历史模式配置  
 app.use(
   historyApiFallback({
     whiteList: ['/api']
   })
 )
-// 代理
+
+// 代理,相当于我们使用Vue、React 中的Proxy 代理
 app.use(
   proxy({
     targets: {
@@ -30,8 +30,10 @@ app.use(
     }
   })
 )
+
 // 静态资源配置
-app.use(serve(__dirname + '/doc'))
-app.listen(10005, () => {
-  console.log('3333 start')
+app.use(serve(__dirname + '/dist')) //__dirname表示当前文件的绝对路径，此处的dist 就是我们前端打包完成后的资源文件夹
+
+app.listen(10005, () => {//配置我们需要监听的服务器端口
+  console.log('10005 start')
 })
